9064c4050f8f3392877fce462b6ac2af77a98205
Allie LeFever <alefever39@gmail.com>
2022-06-27 16:57:49 -0600

    commit tree 77229dd421a2c6b76366489ffa02fdda4ba3f4d0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(match_list)
+    match_list.filter do |possible_match|
+      split_word = @word.split("")
+      split_possible = possible_match.split("")
+      split_word.sort == split_possible.sort
+    end
+  end
+end
